03ed223c93266c1488bd20b0e03d8b9c2463e26f
Le Minh Huy <vegakinvietnam@gmail.com>
2024-08-01 17:58:45 +0700

    part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  http = inject(HttpClient);
+  users: any;
+  
+  ngOnInit(): void {
+    this.http.get('https://localhost:7022/api/Users').subscribe({
+      next: (data) => {
+        this.users = data;
+      },
+      error: (error) => console.error(error),
+      complete: () => {
+        console.log('Kết nối thành công');
+        
+      },
+    })
+  }
 }
